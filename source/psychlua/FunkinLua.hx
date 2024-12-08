96bcadb9b299436afb6928ab7a32614880c1d7c1
Prohack101010 <miracberaber@gmail.com>
2024-12-09 02:39:10 +0300

    Classic

--- source/psychlua/FunkinLua.hx
+++ source/psychlua/FunkinLua.hx
@@ -1806,7 +1806,7 @@ class FunkinLua {
 	#if (MODS_ALLOWED && !flash && sys)
 	public var runtimeShaders:Map<String, Array<String>> = new Map<String, Array<String>>();
 	#end
-	function initLuaShader(name:String)
+	public function initLuaShader(name:String)
 	{
 		if(!ClientPrefs.data.shaders) return false;
 		#if (!flash && sys)
